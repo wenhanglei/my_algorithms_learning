b96f60e640ce20f81e1e0b8fe7a14cad5e54061f
wenhanglei <3188744608@qq.com>
2017-09-19 16:34:04 +0800

    选择排序实现

---
+++ src/Example.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+
+public class Example {
+	/**
+	 * 排序算法
+	 */
+	public void sort(Comparable[] a) {}
+	/**
+	 * 比较大小
+	 */
+	private boolean less(Comparable a, Comparable b) {
+		return false;
+	}
+	/**
+	 * 交换位置
+	 * @param arr
+	 */
+	private void exch(Comparable[] arr) {}
+	/**
+	 * 显示排序结果
+	 */
+	private void show(){}
+	/**
+	 * 测试用的主函数
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String[] arr = scan.nextLine().split(" ");
+		Example s = new Example();
+		s.sort(arr);
+		s.show();
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
