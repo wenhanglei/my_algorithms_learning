2cd69b09402f00edd1984dd7f500d86b22f3a55d
wenhanglei <3188744608@qq.com>
2017-09-23 17:00:44 +0800

    归并排序的粗糙版

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -3,20 +3,22 @@ import java.util.Arrays;
 
 public class MergeSort {
 	/**
+	 * 辅助数组
+	 */
+	private static Comparable[] aux;
+	/**
 	 * 排序算法
 	 */
 	public void sort(Comparable[] a) {
-		int N = a.length;
-		int n = (int)(Math.log(2*N+1)/Math.log(3));
-		for (int i = n; i > 0; i--){
-			int j = (int)((Math.pow(3, i)-1)/2);
-			for(int k = 0; k < j; k++){
-				for(int l = k+j; l < N; l += j){
-					for(int m = l; m > k && less(a[m], a[m-j]); m -= j)
-						exch(a, m, m-j);
-				}
-			}
-		}
+		aux = new Comparable[a.length];
+		sort(a, 0, a.length-1);
+	}
+	private void sort(Comparable[] a, int lo, int hi){
+		if(lo == hi){ return;}
+		int N =lo+hi;
+		sort(a, lo, N/2);
+		sort(a, N%2 == 0?N/2 : N/2+1, hi);
+		merge(a, lo, N/2, hi);
 	}
 	/**
 	 * 归并方法
@@ -27,21 +29,19 @@ public class MergeSort {
 	 * @return
 	 */
 	public static void merge(Comparable[] a, int lo, int mid, int hi) {
-		Comparable[] aux = new Comparable[a.length];
 		for(int i = 0; i < a.length; i++)
 			aux[i] = a[i];
 		int i = lo;
-		int j = lo;
 		int k = mid+1;
-		for (;j < hi+1;j++){
+		for (int j = i;j < hi+1;j++){
 			if(i < mid+1 && k < hi+1 && less(aux[i], aux[k])){
-				a[j] = aux[i]; i++;
+				a[j] = aux[i++]; 
 			}else if(i < mid+1 && k < hi+1){
 				a[j] = aux[k]; k++;
 			}else if(i == mid+1){
-				a[j] = aux[k]; k++;
+				a[j] = aux[k++]; 
 			}else{
-				a[j] = aux[i]; i++;
+				a[j] = aux[i++];
 			}
 		}
 	}
@@ -77,9 +77,10 @@ public class MergeSort {
 	 */
 	public static void main(String[] args) {
 //		String[] data = {"S", "O", "R", "T", "E", "X", "A", "M", "P", "L", "E"};
-//		Integer[] data = {3, 1, 2, 5, 7, 6, 4};
-		String[] test = {"E", "E", "G", "M", "R", "A", "C", "E", "R", "T"};
-		merge(test, 0, 4, 9);
-		System.out.println(Arrays.toString(test));
+		Integer[] data = {3, 1, 2, 5, 7, 6, 4};
+//		String[] test = {"E", "E", "G", "M", "R", "A", "C", "E", "R", "T"};
+		MergeSort ms = new MergeSort();
+		ms.sort(data);
+		System.out.println(Arrays.toString(data));
 	}
 }
