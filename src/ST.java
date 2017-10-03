a23b6b22216efbb4c9347b80f912a3a6ea9fe40b
wenhanglei <3188744608@qq.com>
2017-10-03 15:07:00 +0800

    添加符号表的抽象基类和测试函数

---
+++ src/ST.java
@@ -0,0 +1,75 @@
+
+public class ST<Key, Value> {
+	/**
+	 * 符号表的初始化函数
+	 */
+	ST() {}
+	/**
+	 * 保存键值对
+	 * @param key
+	 * @param val
+	 */
+	void put(Key key, Value val) {}
+	/**
+	 * 获取对应键的值
+	 * @param key
+	 * @return
+	 */
+	Value get(Key key) {
+		return null;
+	}
+	/**
+	 * 删除键值对
+	 * @param key
+	 */
+	void delete(Key key) {}
+	/**
+	 * 是否含有健为key的键值对
+	 * @param key
+	 * @return
+	 */
+	boolean contains(Key key) {
+		return false;
+	}
+	/**
+	 * 符号表是否为空
+	 * @return
+	 */
+	boolean isEmpty() {
+		return false;
+	}
+	/**
+	 * 返回符号表的大小
+	 * @return
+	 */
+	int size() {
+		return 0;
+	}
+	/**
+	 * 返回key迭代器
+	 * @return
+	 */
+	Iterable<Key> keys() {
+		return null;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
