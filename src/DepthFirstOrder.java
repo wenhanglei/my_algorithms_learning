0093a8f8204b06dfeb66bc901ac75b87e09128a5
wenhanglei <3188744608@qq.com>
2017-10-29 18:21:05 +0800

    拓扑排序的实现

---
+++ src/DepthFirstOrder.java
@@ -0,0 +1,41 @@
+
+public class DepthFirstOrder {
+	
+	private boolean[] marked;
+	private Queue<Integer> preOrder;
+	private Queue<Integer> postOrder;
+	private Stack<Integer> reversePostOrder;
+	
+	public DepthFirstOrder(Digraph G) {
+		marked = new boolean[G.V()];
+		preOrder = new Queue<Integer>();
+		postOrder = new Queue<Integer>();
+		reversePostOrder = new Stack<Integer>();
+		for(int i = 0; i < G.V(); i++)
+			if(!marked[i]) dfs(G, i);
+	}
+	private void dfs(Digraph G, int x) {
+		marked[x] = true;
+		preOrder.enqueue(x);
+		for(int v : G.adj(x)) {
+			if(!marked[v]) {
+				dfs(G, v);
+			}
+		}
+		postOrder.enqueue(x);
+		reversePostOrder.push(x);
+	}
+	
+	public Iterable<Integer> pre() {
+		return preOrder;
+	}
+	
+	public Iterable<Integer> post() {
+		return postOrder;
+	}
+	
+	public Iterable<Integer> reversePost() {
+		return reversePostOrder;
+	}
+
+}
