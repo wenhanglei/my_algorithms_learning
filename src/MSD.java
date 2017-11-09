cd832ea0161f5b4ff9966b3e4bc15ffde1cb09af
wenhanglei <3188744608@qq.com>
2017-11-09 14:50:53 +0800

    MSD最高有效位优先字符串排序实现

--- src/MSD.java
+++ src/MSD.java
@@ -1,42 +1,31 @@
 
 public class MSD {
 	
-	//传入的字符串数组
-	private String[] a;
 	//字母表中字母总数
-	private int R = 256;
+	private static int R = 256;
+	private static String[] aux;
 	//字母表
-	private Alphabet alpha;
-	
-	/**
-	 * 最高有效位字符串排序构造函数
-	 * @param a
-	 */
-	public MSD(String[] a, Alphabet alpha) {
-		this.a = a;
-		this.alpha = alpha;
-	}
+	private static Alphabet alpha;
+
 	
 	/**
 	 * 最高有效位字符串排序实现
 	 */
-	public void sort() {
-		int[] count = sort(0, a.length, 1);
-		for(int i = 0; i < count.length; i++){
-			sort(count[i], count[i+1], 2);
-		}
+	public static void sort(String[] a) {
+		if(a == null) return;
+		
 	}
 	
-	private int[] sort(int lo, int hi, int w){
+	private static void sort(String[] a, int lo, int hi, int index){
 		//获取字符串数组的大小
-		int N = hi-lo+1;
+		int N = hi-lo;
 		//创建辅助数组
-		String[] aux = new String[N];
+		aux = new String[N];
 		//创建频率计数数组
 		int[] count = new int[R+2];
 		//获取最高有效位基数为R的索引
 		for(int i = 0; i < N; i++) {
-			int msd = (int)toChar(a[i], w-1)+1;
+			int msd = (int)toChar(a[i], index)+1;
 			count[msd+1]++;
 		}
 		//计算最低有效位的下标
@@ -45,17 +34,20 @@ public class MSD {
 		}
 		//对辅助数组进行排序
 		for(int i = 0; i < N; i++) {
-			int msd = (int)toChar(a[i], w-1)+1;
+			int msd = (int)toChar(a[i], index)+1;
 			aux[count[msd]++] = a[i];
 		}
 		//修改原数组中的排序
 		for(int i = 0; i < N; i++) {
 			a[i] = aux[i];
 		}
-		return count;
+		
+		for(int i = 0; i < count.length; i++) {
+			sort(a, count[i], count[i+1], index+1);
+		}
 	}
 	
-	private int toChar(String s, int i) {
+	private static int toChar(String s, int i) {
 		if(i > s.length()) return -1;
 		return s.charAt(i);
 	}
