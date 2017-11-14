bddf2d7b9b830dc0b11c84ddda64eb14642eb508
wenhanglei <3188744608@qq.com>
2017-11-14 14:31:49 +0800

    KMP子字符串查找算法实现

--- src/KMP.java
+++ src/KMP.java
@@ -2,20 +2,37 @@
 public class KMP {
 	
 	private static int R;
+	//有限自动状态机
+	private int[][] dfa;
+	private String pat;
 	
-	public static void search(String text, String pat) {
-		int[][] dfa = new int[R][pat.length()];
-		
-		int x = 0;
-		for(int i = 0; i < pat.length();) {
-			for(int j = 0; j < R; j++) {
-				dfa[j][i] = dfa[i][x];
-			}
-			dfa[pat.charAt(i)][i] = ++i;
-			x = dfa[pat.charAt(i)][x];
+	/**
+	 * 构造函数
+	 * @param pat
+	 */
+	public KMP(String pat) {
+		this.pat = pat;
+		R = 256;
+		int M = pat.length();
+		dfa[pat.charAt(0)][0] = 1;
+		for(int x = 0, j = 1; j < M; j++) {
+			for(int i = 0; i < R; i++) 
+				dfa[i][j] = dfa[i][x];
+			dfa[pat.charAt(j)][j] = j+1;
+			x = dfa[pat.charAt(j)][x];
 		}
 		
-		
+	}
+	
+	public int search(String text) {
+		int N = text.length();
+		int M = pat.length();
+		int index = N;
+		for(int i = 0, j = 0; i < N-M+1; i++) {
+			if(j == M) { index = i-M; break; }
+			j = dfa[text.charAt(i)][j];
+		}
+		return index;
 	}
 	
 }
