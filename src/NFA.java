fc770ebb296d2c85bced4ca09f733e36e9d1dcd4
wenhanglei <3188744608@qq.com>
2017-11-16 15:02:45 +0800

    NFA算法实现

--- src/NFA.java
+++ src/NFA.java
@@ -1,12 +1,44 @@
 
-public class SimulateNFA {
+public class NFA {
 	
 	//表示无限自动状态机的有向图
 	private Digraph G;
 	//用于匹配的模版的字符数或自动状态机的总状态数
 	private int M;
 	//用于匹配的正则表达式
-	private int[] re;
+	private char[] re;
+	
+	/**
+	 * 无限自动状态机的构造
+	 * @param regexp
+	 */
+	public NFA(String regexp) {
+		Stack<Integer> ops = new Stack<Integer>();
+		re = regexp.toCharArray();
+		M = re.length;
+		G = new Digraph(M+1);
+		
+		for(int i = 0; i < M; i++) {
+			int lp = i;
+			if(re[i] == '(' || re[i] == '|')
+				ops.push(i);
+			else if(re[i] == ')') {
+				int or = ops.pop();
+				if(re[or] == '|') {
+					lp = ops.pop();
+					G.addEdge(lp, or+1);
+					G.addEdge(or, i);
+				}
+				else lp = or;
+			}
+			if(i < M-1 && re[i+1] == '*'){
+				G.addEdge(lp, i+1);
+				G.addEdge(i+1, lp);
+			}
+			if(re[i] == '(' || re[i] == '*' || re[i] == ')')
+				G.addEdge(i, i+1);
+		}
+	}
 	
 	/**
 	 * @param text
