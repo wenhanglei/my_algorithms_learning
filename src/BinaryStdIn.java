bcc129137fd22124d0e40186ffc0df229610bc14
wenhanglei <3188744608@qq.com>
2017-11-17 14:51:03 +0800

    定义二进制输入输出流api

---
+++ src/BinaryStdIn.java
@@ -0,0 +1,27 @@
+/**
+ * 二进制输入流api
+ * @author Administrator
+ *
+ */
+public class BinaryStdIn {
+	//读取一位数据返回为boolean值
+	public boolean readBoolean() {
+		return false;
+	}
+	//读取8bit数据返回为char
+	public char readChar() {
+		return 0;
+	}
+	//读取1~16bit的数据返回为char
+	public char readChar(int bit) {
+		return 0;
+	}
+	//字节流是否为空
+	public boolean isEmpty() {
+		return false;
+	}
+	//关闭流
+	public void close() {
+		
+	}
+}
---
+++ src/BinaryStdOut.java
@@ -0,0 +1,35 @@
+/**
+ * 二进制输出流
+ * @author Administrator
+ *
+ */
+public class BinaryStdOut {
+	/**
+	 * 输出一个bit
+	 * @param bit
+	 */
+	public void write(boolean bit) {
+		
+	}
+	/**
+	 * 输出一个8bit字符
+	 * @param c
+	 */
+	public void write(char c) {
+		
+	}
+	/**
+	 * 输出一个1~16bit的字符
+	 * @param c
+	 * @param r
+	 */
+	public void write(char c, int r) {
+		
+	}
+	/**
+	 * 关闭流
+	 */
+	public void close() {
+		
+	}
+}
