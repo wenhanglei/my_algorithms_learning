f6d70fc0e2527f957731076d434a0bcdf7f7501d
wenhanglei <3188744608@qq.com>
2017-11-19 14:54:45 +0800

    向控制台打印二进制流

--- src/BinaryStdIn.java
+++ src/BinaryStdIn.java
@@ -5,23 +5,23 @@
  */
 public class BinaryStdIn {
 	//读取一位数据返回为boolean值
-	public boolean readBoolean() {
+	public static boolean readBoolean() {
 		return false;
 	}
 	//读取8bit数据返回为char
-	public char readChar() {
+	public static char readChar() {
 		return 0;
 	}
 	//读取1~16bit的数据返回为char
-	public char readChar(int bit) {
+	public static char readChar(int bit) {
 		return 0;
 	}
 	//字节流是否为空
-	public boolean isEmpty() {
+	public static boolean isEmpty() {
 		return false;
 	}
 	//关闭流
-	public void close() {
+	public static void close() {
 		
 	}
 }
