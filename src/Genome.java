ed667f1844c5b1913ce90c344101d278ec104760
wenhanglei <3188744608@qq.com>
2017-11-25 15:01:41 +0800

    完成基础压缩算法demo

--- src/Genome.java
+++ src/Genome.java
@@ -1,9 +1,5 @@
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintStream;
 
 /**
  * 遗传学基因编码数据的数据压缩和恢复
@@ -24,15 +20,32 @@ public class Genome {
 //			char c = BinaryStdIn.readChar();
 //			
 //		}
-		BinaryStdOut.write(0, 2);
+		while(!BinaryStdIn.isEmpty()) {
+			char c = BinaryStdIn.readChar();
+			switch(c) {
+			case 'A': BinaryStdOut.write(0, 2);break;
+			case 'C': BinaryStdOut.write(1, 2);break;
+			case 'G': BinaryStdOut.write(2, 2);break;
+			case 'T': BinaryStdOut.write(3, 2);break;
+			}
+		}
 		BinaryStdOut.flush();
 	}
 	/**
 	 * 数据恢复方法
 	 */
 	public static void extend() {
-		
+		while(!BinaryStdIn.isEmpty()) {
+			int i = BinaryStdIn.readInt(2);
+			switch(i) {
+			case 0: System.out.print('A');break;
+			case 1: System.out.print('C');break;
+			case 2: System.out.print('G');break;
+			case 3: System.out.print('T');break;
+			}
+		}
 	}
+	
 	/**
 	 * 测试方法
 	 * @param args
@@ -43,7 +56,7 @@ public class Genome {
 //		else if(args[0] == "+") extend(); //如果是加好则恢复文件
 		//读取文件流
 		//包装文件流
-		 BufferedInputStream is = new BufferedInputStream(
+	/*	 BufferedInputStream is = new BufferedInputStream(
 						new FileInputStream("/e:/think_in_java/test.txt"));
 		//保存标准输入流
 		InputStream intmp = System.in;
@@ -53,6 +66,16 @@ public class Genome {
 		PrintStream os = new PrintStream(
 				new FileOutputStream("/e:/think_in_java/data.txt"));
 		System.setOut(os);
-		compress();
+		compress();*/
+		//######################################################
+//		BufferedInputStream is = new BufferedInputStream(
+//				new FileInputStream("/e:/think_in_java/data.txt"));
+//		System.setIn(is);
+//		extend();
+		//######################################################
+		File inFile = new File("/e:/think_in_java/test.txt");
+		File outFile = new File("/e:/think_in_java/data.txt");
+		System.out.println("输入文件的大小为：" + inFile.length());
+		System.out.println("输出文件的大小为：" + outFile.length());
 	}
 }
