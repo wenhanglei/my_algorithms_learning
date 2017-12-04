82f9077bcf4e163b0c0009f543b6e45ca200c52f
wenhanglei <3188744608@qq.com>
2017-12-04 14:39:04 +0800

    实现LZW压缩算法

--- src/LZW.java
+++ src/LZW.java
@@ -4,8 +4,12 @@
  *
  */
 public class LZW {
-	//用于查找最长子串的字典树符号表
-//	public static TrieST<Integer> codeword = new TrieST<Integer>(256);
+	//输入字符的数量
+	private static final int R = 256;
+	//编码后的编码字符的数量
+	private static final int L = 4096;
+	//编码位宽
+	private static final int W = 12;
 	/**
 	 * 压缩算法
 	 */
@@ -18,17 +22,58 @@ public class LZW {
 		 * 		* 如果编码表中的索引用完则不在装载
 		 */
 		//获取字符串
-		String s = BinaryStdIn.readString();
+		String input = BinaryStdIn.readString();
 		//初始化字典树
-//		for(int i = 41; i < 80; i++) 
-//			codeword.put(String.valueOf(i), i);
-		
+		TrieST<Integer> st = new TrieST<Integer>();
+		for(int i = 0; i < R; i++) {
+			st.put("" + (char)i, i);
+		}
+		//R表示文件尾
+		int code = R+1;
+		while(input.length() > 0) {
+			//获取当前字典树中最长子串
+			String s = st.longestPrefixOf(input);
+			//向标准输出中打印编码后的子串
+			BinaryStdOut.write(st.get(s), W);
+			int t = s.length();
+			//将s添加到符号表
+			if(t < input.length() && code < L)
+				st.put(input.substring(0,t+1), code++);
+			input = input.substring(t);
+		}
+		//打印eof符号
+		BinaryStdOut.write(R);
+		//关闭输出连接
+		BinaryStdOut.close();
 	}
 	/**
 	 * 恢复算法
 	 */
 	public static void expand() {
-		
+		//构造用于解码的字符串数组
+		String[] st = new String[L];
+		//下一个可用的编码值
+		int i;
+		//使用字符初始化字符串数组
+		for(i = 0; i < R; i++) 
+			st[i] = "" + (char)i;
+		st[i++] = " ";
+		int codeword = BinaryStdIn.readInt(W);
+		String val = st[codeword];
+		while(true) {
+			//向标准输出打印当前子串
+			BinaryStdOut.write(val);
+			codeword = BinaryStdIn.readInt(W);
+			//如果碰到eof符号停止运行
+			if (codeword == R) break;
+			String s = st[codeword];
+			if(i == codeword)
+				s = val + val.charAt(0);
+			if(i < L)
+				st[i++] = val + s.charAt(0);
+			val = s;
+		}
+		BinaryStdOut.close();
 	}
 	
 	public static void main(String[] args) {
