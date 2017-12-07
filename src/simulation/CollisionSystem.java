3d28baa61fb417310bd6ae04c1c1c90c51151b68
wenhanglei <3188744608@qq.com>
2017-12-07 16:43:03 +0800

    实现碰撞预测函数

--- src/simulation/CollisionSystem.java
+++ src/simulation/CollisionSystem.java
@@ -1,5 +1,23 @@
 package simulation;
 
 public class CollisionSystem {
-
+	/**
+	 * 碰撞预测函数
+	 * @param a
+	 * @param limit
+	 */
+	private void predictCollisions(Particle a, double limit) {
+		if(a == null) return;
+		for(int i = 0; i < particles.length; i++) {
+			double dt = a.timeTOHit(particles[i]);
+			if(t + dt <= limit)
+				pq.inset(new Event(t + dt, a, particles[i]));
+		}
+		double dtX = a.timeToHitVerticalWall();
+		if(t + dtX <= limit)
+			p.inset(new Event(t + dtX, a, null));
+		double dtY = a.timeToHitHorizontalWall();
+		if(t + dtY <= limit)
+			pq.insert(new Event(t + dtY, null, a));
+	}
 }
