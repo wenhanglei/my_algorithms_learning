acafb2e41931cc09f24ac9fb170190f922b00196
wenhanglei <3188744608@qq.com>
2018-02-01 18:40:32 +0800

    最大流问题

---
+++ src/max_flow/FlowNetwork.java
@@ -0,0 +1,63 @@
+package max_flow;
+
+import java.util.List;
+
+/**
+ * 流网络
+ * @author wenhanglei
+ */
+public class FlowNetwork {
+	
+	//存储所有边的邻接链表
+	private List<FlowEdge>[] adj;
+	//顶点个数
+	private int V;
+	//边的个数
+	private int E;
+	
+	/**
+	 * 顶点个数为v的空流网络构造函数
+	 * @param V
+	 */
+	public FlowNetwork(int V) {
+		adj = (List<FlowEdge>[]) new Object[V];
+		this.V = V;
+	}
+	
+	/**
+	 * @return 该流网络的顶点的个数
+	 */
+	public int V() {
+		return V;
+	}
+	
+	/**
+	 * @return 返回当前流网络的边的条数
+	 */
+	public int E() {
+		return E;
+	}
+	
+	/**
+	 * 向当前流网络中添加一个边e
+	 * @param e
+	 */
+	public void addEdge(FlowEdge e) {
+		
+	}
+	
+	/**
+	 * @param v
+	 * @return 顶点v指向的所有边的迭代器
+	 */
+	public Iterable<FlowEdge> adj(int v) {
+		return null;
+	}
+	
+	/**
+	 * @return 当前网络流中所有边
+	 */
+	public Iterable<FlowEdge> edges() {
+		return null;
+	}
+}
---
+++ src/max_flow/MaxFlowClient.java
@@ -0,0 +1,13 @@
+package max_flow;
+
+public class MaxFlowClient {
+	
+	private boolean localEq(FlowNetwork G, int v){
+		return false;
+	}
+	
+	private boolean isFeasible(FlowNetwork G){
+		return false;
+	}
+
+}
