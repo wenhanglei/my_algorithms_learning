c25c93430b00c92865e13cd17b110d7e46d9091a
wenhanglei <wenhanglei@gmail.com>
2018-05-09 21:22:25 +0800

    深度优先遍历判断有向图是否为无环图

--- src/graphics/DirectedCycle.java
+++ src/graphics/DirectedCycle.java
@@ -1,37 +1,57 @@
 package graphics;
+
+import java.util.Stack;
+
 /**
  * 有向图是否有环的判定类
  * @author wenhanglei
  */
 public class DirectedCycle {
 	
-	private Digraph G;
-	private boolean[] marked;         //顶点标记数组
-	private int[] next;               //顶点遍历顺序
+	private boolean[] marked;           //顶点标记数组
+	private int[] edgeTo;               //顶点遍历顺序
+	private boolean[] onStack;          //是否有环的标记
+	private Stack<Integer> cycle;       //保存构成环的顶点
 	
 	public DirectedCycle(Digraph G) {
-		this.G = G;
 		marked = new boolean[G.V()];
-		next = new int[G.V()];
+		edgeTo = new int[G.V()];
+		onStack = new boolean[G.V()];
 	}
 	
 	/**
 	 * 有向环是否存在
 	 */
 	public boolean hasCycle(){
-		return false;
+		return cycle != null;
 	}
 	
 	/**
 	 * 有向图的深度优先遍历
 	 */
-	private void DirectedDFS(){
+	private void DirectedDFS(Digraph G, int w){
+		if(hasCycle()) return;
+		marked[w] = true;
+		onStack[w] = true;
+		for(int v : G.adj(w)){
+			if(!marked[v]){
+				DirectedDFS(G, v);
+			}else if(onStack[v]){
+				cycle = new Stack<>();
+				cycle.push(v);
+				while(edgeTo[w] != v){
+					cycle.push(w);
+					w = edgeTo[w];
+				}
+			}
+		}
+		onStack[w] = false;
 	}
 	
 	/**
 	 * 有向环的所有顶点
 	 */
 	public Iterable<Integer> cycle(){
-		return null;
+		return cycle;
 	}
 }
